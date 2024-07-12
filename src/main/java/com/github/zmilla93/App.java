54c12fc87fdcbfaace15603a74db2349d3a82b01
Zach Miller <zmilla93@gmail.com>
2024-07-12 17:29:41 -0500

    init

---
+++ src/main/java/com/github/zmilla93/App.java
@@ -0,0 +1,17 @@
+package com.github.zmilla93;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static void doCoolStuff(){
+        System.out.println("Cool Stuff!");
+    }
+}
